a443206bb343fece3d64ef1f830c1b489f155b25
TheKhaliqAnsari <khaliqansari771@gmail.com>
2022-11-28 04:06:29 +0530

    Created Banner and design it completly.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1> Thekhaliqansari
-      </h1>
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,77 @@
+import React,{ useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+function Banner() {
+
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Front-end Developer", "Problem Solver", "Tech Enthusiast"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(200 - Math.random() * 100);
+    const period = 2000;
+
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => {clearInterval(ticker)};
+    }, [text])
+
+    const tick = () =>{
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if(isDeleting){
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Porfolio</span>
+            <h1>
+              {`Hi I'm Khaliq Ansari `}
+              <span className="wrap">{text}</span>
+            </h1>
+            <p>
+              Detailed and oriented programmer & a computer science engineer. I
+              have been working on technologies such as C++, HTML/CSS/JS, React,
+              SQL, Linux. Also using Data structures and algorithms for
+              effectively organizing data in computers. Have worked on multiple
+              practical projects and have been exploring new technologies and
+              leveraging them to solve real life problems.
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Let's connect <ArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default Banner;
