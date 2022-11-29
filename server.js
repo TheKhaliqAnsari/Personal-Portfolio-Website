dfcaae4fb40811b6831b2067f4078b96a8253aa8
TheKhaliqAnsari <khaliqansari771@gmail.com>
2022-11-29 12:49:38 +0530

    Made contact form deployment ready.

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
+require("dotenv").config()
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
