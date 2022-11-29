595d122c33e7235c8d9655739d33ae1b8f25048c
TheKhaliqAnsari <khaliqansari771@gmail.com>
2022-11-30 00:29:09 +0530

    Final Release

--- server.js
+++ server.js
@@ -1,54 +1,54 @@
 // Not using this server. -> I uploaded mail server on aws and using mailing service from there.
 
 
-// const express = require("express");
-// const router = express.Router();
-// const cors = require("cors");
-// const nodemailer = require("nodemailer");
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
-// require("dotenv").config()
-// // server used to send send emails
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use("/", router);
-// app.listen(5000, () => console.log("Server Running"));
+require("dotenv").config()
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
 
-// const contactEmail = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: "thekhaliqansari+porfolio@gmail.com",
-//     pass: process.env.REACT_APP_SECRET_KEY
-//   },
-// });
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "thekhaliqansari+porfolio@gmail.com",
+    pass: process.env.REACT_APP_SECRET_KEY
+  },
+});
 
-// contactEmail.verify((error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Ready to Send");
-//   }
-// });
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
 
-// router.post("/contact", (req, res) => {
-//   const name = req.body.firstName + " " + req.body.lastName ;
-//   const email = req.body.email;
-//   const message = req.body.message;
-//   const phone = req.body.phone;
-//   const mail = {
-//     from: name,
-//     to: "thekhaliqansari+portfolio@gmail.com",
-//     subject: "Contact Form Submission - Portfolio",
-//     html: `<p>Name: ${name}</p>
-//            <p>Email: ${email}</p>
-//            <p>Phone: ${phone}</p>
-//            <p>Message: ${message}</p>`,
-//   };
-//   contactEmail.sendMail(mail, (error) => {
-//     if (error) {
-//       res.json(error);
-//     } else {
-//       res.json({ code: 200, status: "Message Sent" });
-//     }
-//   });
-// });
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + " " + req.body.lastName ;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "thekhaliqansari+portfolio@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
